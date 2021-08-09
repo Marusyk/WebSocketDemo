94f2baa728a0e4860520a143ec78577638c5174e
Roman Marusyk <romamarusyk@gmail.com>
2021-08-09 23:54:35 +0300

    Init

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+
+            try
+            {
+                var client = new ClientWebSocket();
+                client.Options.KeepAliveInterval = TimeSpan.FromMinutes(5);
+                client.Options.SetRequestHeader("Authorization", "token");
+                client.Options.SetRequestHeader("CustomAuthorization", "custom-token");
+                await client.ConnectAsync(new Uri("ws://localhost:5000/heartbeats"), CancellationToken.None);
+                Console.WriteLine("Connected!");
+
+                var heartbeat = new
+                {
+                    Timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds(),
+                };
+                var json = JsonSerializer.Serialize(heartbeat);
+                var bytes = Encoding.UTF8.GetBytes(json);
+
+                await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Binary, true,
+                    CancellationToken.None);
+
+                await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error: " + e.Message + " " + e.GetType());
+                throw;
+            }
+            Console.WriteLine("Done.");
+        }
+    }
+}
---
+++ WebServer/WebSocketMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    public class WebSocketMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public WebSocketMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.WebSockets.IsWebSocketRequest)
+            {
+                context.Response.StatusCode = StatusCodes.Status505HttpVersionNotsupported;
+                await context.Response.WriteAsync("Only WebSocket requests are allowed at this endpoint.");
+                await context.Response.CompleteAsync();
+                return;
+            }
+
+            Console.WriteLine($"Authorization = {context.Request.Headers["Authorizationn"]}");
+            Console.WriteLine($"CustomAuthorization = {context.Request.Headers["CustomAuthorization"]}");
+
+            WebSocket socket = await context.WebSockets.AcceptWebSocketAsync();
+            /// ...
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IApplicationBuilder MapWebSocket(this IApplicationBuilder app, PathString path)
+        {
+            app.UseWebSockets(new WebSocketOptions
+            {
+                KeepAliveInterval = TimeSpan.FromMinutes(5)
+            });
+            return app.Map(path, _app => _app.UseMiddleware<WebSocketMiddleware>());
+        }
+    }
+}
